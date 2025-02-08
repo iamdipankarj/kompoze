2433c232a4c76e488fc5c5b91dd8031763282478
Dipankar Jana <iamdipankarj@gmail.com>
2025-02-08 17:40:08 +0530

    Delete workflow file and use cli to publish to github pages.

--- scripts/deploy-storybook.js
+++ scripts/deploy-storybook.js
@@ -1,6 +1,6 @@
-const ghpages = require('gh-pages')
+import { publish } from 'gh-pages'
 
-ghpages.publish('storybook-static', { history: false }, err => {
+publish('storybook-static', { history: false }, err => {
   if (err) console.err(err)
   console.log('🚀 Successfully published to https://iamdipankarj.github.io/kompoze')
 })
